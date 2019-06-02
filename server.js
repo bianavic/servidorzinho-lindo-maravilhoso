c4bafd4951c9091ff7f0504a125c1631de46ad72
bianavic <bianavic@gmail.com>
2019-06-02 17:38:36 -0300

    Add delete method

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 
-const express = require('express');
+const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const controller = require('./ComidasController')
 
-const server = express();
+const server = express()
 server.use(cors())
 server.use(bodyParser.json())
 
-server.get("/comidas", (request, response)=>{
+server.get('/comidas', (request, response) => {
   response.send(controller.getAll())
 })
 
 server.post('/comidas', (request, response) => {
-  response.send(controller.add(request.body))
+  controller.add(request.body)
 })
 
-/* server.delete('/comidas/:id', (request, response) => {
+server.delete('/comidas/:id', (request, response) => {
   controller.remove(request.params.id)
   response.sendStatus(204)
-}) */
+})
 
-server.listen(3000);
-console.log('Servidor rodando na porta 3000');
+server.listen(3000)
+console.log('Servidor rodando na porta 3000')
