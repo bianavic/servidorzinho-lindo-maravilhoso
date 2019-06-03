e3e9b564751ff2168252eea8a45472f5f6f03315
bianavic <bianavic@gmail.com>
2019-06-03 13:01:29 -0300

    Delete with express

--- ComidasController.js
+++ ComidasController.js
@@ -1,22 +1,20 @@
-
-const repository = require('/ComidasRepositorio')
+const repository = require('./ComidasRepository')
 
 const getAll = () => {
   return repository.comidas
 }
 
 const add = (comida) => {
-  comida.id = Math.random().toString(36).substr(-8)  
+  comida.id = Math.random().toString(36).substr(-8)
   getAll().comidas.push(comida)
 }
 
 const remove = (id) => {
   let comidasQueFicaram = getAll()
 
-  getAll().comidas = comidasQueFicaram.comidas.filter((comida => {
+  getAll().comidas = comidasQueFicaram.comidas.filter((comida) => {
     return comida.id !== id
-})
-  )
+  })
 }
 
 module.exports = {
